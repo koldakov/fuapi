dedf0e55778daa0c92263c48eccbba67e4ddf076
Ivan Koldakov <ivan.koldakov@gmail.com>
2026-01-31 22:01:21 +0200

    Add project root path

--- src/fuapi/core/_settings.py
+++ src/fuapi/core/_settings.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from pydantic import HttpUrl
 from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
     base_url: HttpUrl = HttpUrl("https://futuramaapi.com")
+
+    @property
+    def project_root(self) -> Path:
+        return Path(__file__).parent.parent.parent.parent.resolve()
 
 
 settings: Settings = Settings()
